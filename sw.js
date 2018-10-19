fce1290ebd8d757aa9d07ef5bcf1a13b91332a85
Lucas Huguet <lucashuguet31@gmail.com>
2018-10-19 22:52:57 +0200

    Cache Fetching

--- sw.js
+++ sw.js
@@ -24,17 +24,25 @@ self.addEventListener('install', e => {
 })
 
 self.addEventListener('activate', e => {
-    console.log('Service Worker activated')
+    console.log('Service Worker activated');
 
     e.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache')
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
                     }
                 })
             )
         })
+    )
+})
+
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch
     )
 })
