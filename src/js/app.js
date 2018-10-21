d662648f9455a53c2e2155c5be76c40a1c18c8aa
Lucas Huguet <lucashuguet31@gmail.com>
2018-10-21 14:14:45 +0200

    deplacement sw.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ $('.m-nav-toggle').click(function(e){
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../../sw.js')
+            .register('./sw.js')
             .then(reg => console.log('Service Worker regestered'))
             .catch(err => console.log(`Service Worker : error : ${err}`))
     })
