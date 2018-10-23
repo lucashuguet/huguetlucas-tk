b808842d9e00b605144c67b56257b73cfcbcfecb
Lucas Huguet <lucashuguet31@gmail.com>
2018-10-23 20:06:29 +0200

    Manifest

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,3 @@ $('.m-nav-toggle').click(function(e){
     $('.m-nav-toggle').toggleClass('is-open');
 })
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('./sw.js')
-            .then(reg => console.log('Service Worker regestered'))
-            .catch(err => console.log(`Service Worker : error : ${err}`))
-    })
-}
