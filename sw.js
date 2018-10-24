f279e4f1e870f99facb971260cf7b1d5796c5f00
Lucas Huguet <lucashuguet31@gmail.com>
2018-10-24 14:39:59 +0200

    buddle offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('sw-toolbox.js')
-toolbox.precache(['index.html', 'dist/css/style.css'])
+toolbox.precache(['index.html', 'dist/css/style.css', 'dist/buddle.js'])
 
 toolbox.router.get('/*', toolbox.networkFirst, {
-networkTimeoutSeconds: 10})
+networkTimeoutSeconds: 15})
